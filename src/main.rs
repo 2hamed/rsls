08caac59cc12a1271fedce74c6c9e56518fdab95
Hamed Momeni <hamed.ma7@gmail.com>
2020-03-25 14:53:52 +0430

    moving get dir to function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
-use std::env;
+use std::{env, error, io};
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
 
     let dir = if args.len() == 1 {
-        let cur_dir = match env::current_dir() {
-            Ok(v) => match v.to_str() {
-                Some(v) => String::from(v),
-                None => String::from(""),
-            },
-            Err(_e) => String::from(""),
-        };
-        cur_dir
+        getCurrentDir().unwrap_or_else(|error| {
+            panic!("{}", error);
+        })
     } else {
         args.remove(1)
     };
 
     println!("Dir: {}", dir)
+}
+
+fn getCurrentDir() -> Result<String, io::Error> {
+    let dir = env::current_dir()?;
+    let dirStr = dir
+        .to_str()
+        .ok_or(io::Error::from(io::ErrorKind::InvalidData))?;
+    Ok(String::from(dirStr))
 }
