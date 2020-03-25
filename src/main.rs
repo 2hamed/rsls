06a5b9a816488ef5bbf0bd1c3b689f599d23b422
Hamed Momeni <hamed.ma7@gmail.com>
2020-03-25 15:04:38 +0430

    tidy up the code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-use std::{env, error, io};
+use std::{env, io};
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
 
-    let dir = if args.len() == 1 {
-        getCurrentDir().unwrap_or_else(|error| {
+    let dir = match args.len() {
+        1 => get_current_dir().unwrap_or_else(|error| {
             panic!("{}", error);
-        })
-    } else {
-        args.remove(1)
+        }),
+        _ => args.remove(1),
     };
 
     println!("Dir: {}", dir)
 }
 
-fn getCurrentDir() -> Result<String, io::Error> {
+fn get_current_dir() -> Result<String, io::Error> {
     let dir = env::current_dir()?;
-    let dirStr = dir
+    let dir_str = dir
         .to_str()
         .ok_or(io::Error::from(io::ErrorKind::InvalidData))?;
-    Ok(String::from(dirStr))
+    Ok(String::from(dir_str))
 }
