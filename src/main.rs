8bd4dc143f9a761e8658f7984cb784dd0fbfd0d2
Hamed Momeni <hamed.ma7@gmail.com>
2020-03-28 00:42:17 +0430

    getting abs_path and listing files in dir

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-use std::{env, io};
+use std::os::unix::fs::PermissionsExt;
+use std::{env, fs, io};
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
 
-    let dir = match args.len() {
+    let path = match args.len() {
         1 => get_current_dir().unwrap_or_else(|error| {
             panic!("{}", error);
         }),
         _ => args.remove(1),
     };
 
-    println!("Dir: {}", dir)
+    let abs_path = determine_abs_path(&path[..], &get_current_dir().unwrap()[..]);
+
+    match fs::read_dir(abs_path) {
+        Err(e) => panic!("{}", e),
+        Ok(dir) => {
+            for f in dir {
+                match f {
+                    Ok(entry) => println!("{:o}", entry.metadata().unwrap().permissions().mode()),
+                    Err(e) => panic!("{}", e),
+                }
+            }
+        }
+    }
 }
 
 fn get_current_dir() -> Result<String, io::Error> {
@@ -19,4 +32,18 @@ fn get_current_dir() -> Result<String, io::Error> {
         .to_str()
         .ok_or(io::Error::from(io::ErrorKind::InvalidData))?;
     Ok(String::from(dir_str))
+}
+
+fn determine_abs_path(path: &str, current_dir: &str) -> String {
+    let mut abs_path: String;
+    if path.as_bytes()[0] == b'/' {
+        abs_path = String::from(path);
+        return abs_path;
+    }
+
+    abs_path = String::from(current_dir);
+    abs_path.push_str("/");
+    abs_path.push_str(path);
+
+    abs_path
 }
